198e3b3af886558f41504365f539e221ec7d1da3
Emmanuel-Develops <itakpeemma@gmail.com>
2021-10-07 13:44:22 +0100

    completed popular movies thumbnails

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+const Header = () => (
+        <Wrapper>
+            <Content>
+                <LogoImg src={RMDBLogo} alt='app logo'/>
+                <TMDBLogoImg src={TMDBLogo} alt='tmdb logo'/>
+            </Content>
+        </Wrapper>
+)
+
+export default Header;
