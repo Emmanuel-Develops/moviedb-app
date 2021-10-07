e6a26063a1820ac8804b73ce01801cd85a13f0ab
Emmanuel-Develops <itakpeemma@gmail.com>
2021-10-07 14:19:01 +0100

    wrot spinner style

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -30,6 +31,7 @@ const Home = () => {
                     title={state.results[0].title}
                     text={state.results[0].overview}
                 />
+                // <Spinner />
             : null
             }
             <Grid header='Popular Movies'>
@@ -44,6 +46,7 @@ const Home = () => {
                     />
                 ))}
             </Grid>
+            <Spinner />
         </>
     )
 }
